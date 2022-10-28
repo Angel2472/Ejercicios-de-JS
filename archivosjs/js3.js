05718b7e9f7712449913bdd37b9264b03ea00843
Angel2472 <angel.gzz373@gmail.com>
2022-10-28 16:17:42 -0500

    js3 numeros perfectos

--- archivosjs/js3.js
+++ archivosjs/js3.js
@@ -12,3 +12,25 @@ for (i = 1; i <= cant_tomas; i++) {
     document.write("<br><br>");
     hora += frecuenciatomadiaria;
 }
+
+let Numeros = prompt("(Programa numeros perfectos) ¿Cantidad de numeros: ?");
+let Lista_Numeros = 0;
+
+let numero = 0;
+
+document.write(`<h1>Cantidad de Numeros Perfectos</h1>`);
+document.write(`<h2>Numeros Encontrados: ${Numeros}</h2>`)
+while (Numeros != Lista_Numeros){
+    numero = numero + 1;
+    let sumatoria = 0;
+
+    for (let i = 1; i < numero; i++){
+        if(numero % i == 0 ){
+            sumatoria = sumatoria + i;
+        };
+    };
+    if (sumatoria == numero) {
+        document.write(numero + "<br>");
+        Lista_Numeros = Lista_Numeros + 1;
+    };
+}
