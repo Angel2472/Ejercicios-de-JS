86249674a7e00b7c7c899c63da362277a535abab
Angel2472 <angel.gzz373@gmail.com>
2022-10-28 16:22:16 -0500

    js5 js tablas

--- archivosjs/js5.js
+++ archivosjs/js5.js
@@ -23,3 +23,28 @@ function ejecutarcalculadora() {
     let operacion = document.getElementById("operacion").value;
     document.getElementById("resultado").value = operaciones(primernum, segundonum, operacion);
 }
+
+// Generar tabla
+
+function generar_tabla (numero, tipodetabla, longitudtabla) {
+    let tablafinal = [];
+    switch (parseInt(tipodetabla)) {
+        case 1:
+            for (i=0; i<= longitudtabla; i++) {tablafinal.push(`${numero} x ${i} = ${numero*i}`);}
+            break;
+        case 2:
+            for (i=longitudtabla+1; i> 0; i--) {tablafinal.push(`${numero} x ${i} = ${numero*i}`);}
+            break
+    }
+    return tablafinal;
+}
+
+function tablafinal() {
+    document.getElementById("tabla").innerHTML = "";
+    let nro = parseInt(document.getElementById("nro").value);
+    let tipo = document.getElementById("tipodetabla").value;
+    let longitud = parseInt(document.getElementById("longitud").value);
+    let tabla_obtenida = generar_tabla(nro, tipo, longitud);
+    let lugar_a_colocar = document.getElementById("tabla");
+    for (i=1; i< tabla_obtenida.length; i++) {lugar_a_colocar.innerHTML += tabla_obtenida[i]; lugar_a_colocar.innerHTML += "<br>"}
+}
